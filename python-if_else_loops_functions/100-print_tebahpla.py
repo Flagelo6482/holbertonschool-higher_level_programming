8f66b7da3084aebbaa9671f21bead069673de69c
Flagelo6482 <frankln6482@gmail.com>
2023-06-05 10:35:00 -0700

    feat: Update :rocket:

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # 100-print_tebahpla.py
 
-def smile():
-    """Smile the mirror!"""
-    for i in range(122, 96, -1):
-        if i % 2 != 0:
-            i -= 32
-        print(chr(i), end="")
+"""Smile the mirror!"""
+
+for i in range(122, 96, -1):
+    if i % 2 != 0:
+        i -= 32
+    print(chr(i), end="")
