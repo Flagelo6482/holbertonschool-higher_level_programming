a5065002798627b55fe6fb3a88f84719fa2f081d
Flagelo6482 <frankln6482@gmail.com>
2023-06-05 22:34:20 -0700

    feat: Update :rocket:

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -4,4 +4,4 @@
 def print_list_integer(my_list=[]):
     for i in range(len(my_list)):
         if isinstance(i, int):
-            print("{}".format(my_list[i]))
+            print("{:d}".format(my_list[i]))
