6df0b2f72dd63aee7aca366fe804f08d829f99b2
Flagelo6482 <frankln6482@gmail.com>
2023-06-05 22:58:33 -0700

    feat: Update :rocket:

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,9 +2,7 @@
 # 1-element_at.py
 
 def element_at(my_list, idx):
-    """Access in a list!"""
     if idx < 0 or idx > len(my_list):
         return
     else:
-        indice = my_list[idx]
-        return indice
+        return my_list[idx]
