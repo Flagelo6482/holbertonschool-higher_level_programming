96776ff3c849b8292db5d0604f9ccda51c77fc30
Flagelo6482 <frankln6482@gmail.com>
2023-06-05 22:23:46 -0700

    feat: Function :sparkles:

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 0-print_list_integer.py
+
+def print_list_integer(my_list=[]):
+    """Print list!"""
+    for i in range(len(my_list)):
+        print("{}".format(my_list[i]))
