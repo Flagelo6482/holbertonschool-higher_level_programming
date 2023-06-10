c02eac6034236e0eeeda041a5534c534b951d26a
Flagelo6482 <frankln6482@gmail.com>
2023-06-10 09:02:07 -0700

    feat: Print and count :rocket:

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        nums = 0
+        i = 0
+        while i < x:
+            if x == 0:
+                break
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                nums += 1
+            i += 1
+        print()
+        return nums
+
+    except IndexError:
+        raise
