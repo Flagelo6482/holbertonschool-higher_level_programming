48e5a72ef87259e70e5542ebd918d5398e7817e3
Flagelo6482 <frankln6482@gmail.com>
2023-06-11 16:39:22 -0700

    feat: Update :rocket:

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-# 3-print_reversed_list_integer.py
 
 def print_reversed_list_integer(my_list=[]):
     longitud = len(my_list) - 1
     for longitud in range(longitud, -1, -1):
-        if isinstance(longitud, int):
             print("{:d}".format(my_list[longitud]))
