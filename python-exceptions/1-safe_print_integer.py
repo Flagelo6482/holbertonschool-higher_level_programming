f15ca80e706637b91d8ae87bc5d842cdffe6679b
Flagelo6482 <frankln6482@gmail.com>
2023-06-11 07:13:39 -0700

    feat: Update :rocket:

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,9 +2,8 @@
 
 def safe_print_integer(value):
     try:
-        if isinstance(value, int):
-            print("{:d}".format(value))
-            return True
+        print("{:d}".format(isinstance(value, int)))
+        return True
 
     except ValueError:
         return False
