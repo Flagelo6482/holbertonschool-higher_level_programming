2e39ba340e8416e18c52b73dc159c0d6241303a5
Flagelo6482 <frankln6482@gmail.com>
2023-06-11 18:45:54 -0700

    feat: Update :art:

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_b) == 0:
-        element_1 = tuple_a[0]
-        element_2 = tuple_a[1]
-    elif len(tuple_b) == 1:
-        element_1 = tuple_a[0] + tuple_b[0]
-        element_2 = tuple_a[1] + 0
-    else:
-        element_1 = tuple_a[0] + tuple_b[0]
-        element_2 = tuple_a[1] + tuple_b[1]
+    a = tuple_a + (0,) * (2 - len(tuple_a))
+    b = tuple_b + (0,) * (2 - len(tuple_b))
 
-    return (element_1, element_2)
+    return (a[0] + b[0], a[1] + b[1])
