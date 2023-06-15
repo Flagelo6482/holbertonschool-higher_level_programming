4a2e46939595dc28c43f7771a5104ad7b1aa1ea3
Flagelo6482 <frankln6482@gmail.com>
2023-06-15 14:56:16 -0700

    feat: Ourt first square :rocket:

---
+++ python-classes/5-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+
+"""Printing a square!"""
+
+
+class Square:
+    """Initializing attributes"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    """Property to return a private attribute"""
+    @property
+    def size(self):
+        return self.__size
+
+    """Property to update a private value"""
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    """Function that returns the area of the square!"""
+    def area(self):
+        return self.__size * self.__size
+
+    """Function to print our square!"""
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
