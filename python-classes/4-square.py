a60710476283795ae17b6f1ef3aa10926f190c60
Flagelo6482 <frankln6482@gmail.com>
2023-06-15 14:29:25 -0700

    feat: Update :art:

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -14,13 +14,6 @@ class Square:
         else:
             self.__size = size
 
-    """Function that returns the area of the square!"""
-    def area(self):
-        if not isinstance(self.__size, int):
-            raise TypeError("size must be an integer")
-        else:
-            return self.__size * self.__size
-
     """Property to retun a private attribute"""
     @property
     def size(self):
@@ -29,4 +22,16 @@ class Square:
     """Property to update a private value"""
     @size.setter
     def size(self, value):
-        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    """Function that returns the area of the square!"""
+    def area(self):
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        else:
+            return self.__size * self.__size
