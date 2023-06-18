f914515eeb01cb51da8fd542ee11a0e34f94e9b5
Flagelo6482 <frankln6482@gmail.com>
2023-06-18 06:36:47 -0700

    feat: Update :rocket:

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,8 +10,8 @@ def add_integer(a, b=98):
     """
     Function that returns the sum of two numbers
     """
-    if not isinstance(a, (int, float)):
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
     return (int(a) + int(b))
