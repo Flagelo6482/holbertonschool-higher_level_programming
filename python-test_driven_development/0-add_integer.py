bfdbf7c7a912444ce8240822e26c1253a32bbb36
Flagelo6482 <frankln6482@gmail.com>
2023-06-18 06:27:07 -0700

    feat: Update and proyect Test-driven :sparkles:

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Sum arithmetic
+
+Function
+"""
+
+def add_integer(a, b=98):
+    """
+    Function that returns the sum of two numbers
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
