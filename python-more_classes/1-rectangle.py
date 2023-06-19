552acd81dc51d3d0416e4d0aa2c1a9dd07ef09b7
Flagelo6482 <frankln6482@gmail.com>
2023-06-19 08:33:40 -0700

    feat: Update :rocket:

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -19,18 +19,6 @@ class Rectangle:
         self.__width = width
 
     @property
-    def width(self):
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("value must be >= 0")
-        self.__width = value
-
-    @property
     def height(self):
         return self.__height
 
@@ -41,3 +29,16 @@ class Rectangle:
         elif value < 0:
             raise ValueError("value must be >= 0")
         self.__height = value
+
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("value must be >= 0")
+        self.__width = value
