bba83c22bbee93c7e9e01671d1b2998fc1580cfa
Flagelo6482 <frankln6482@gmail.com>
2023-06-22 09:32:19 -0700

    Feat: Integer validator :rocket:

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+
+"""Integer validator"""
+
+
+class BaseGeometry:
+    """Class with two methods that generate a raise"""
+    def area(self):
+        """
+        Function that generates a raise error
+
+        Raise:
+            Exception:
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Function that checks an integer
+
+        Args:
+            name(string): name
+            value(int)  : integer
+        Raise:
+            TypeError: <name> must be an integer
+            ValueError: <name> must be be greater than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
