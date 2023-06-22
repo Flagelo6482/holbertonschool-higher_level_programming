5ed0a9243015662002d501d43b6eeebdeae9d782
Flagelo6482 <frankln6482@gmail.com>
2023-06-22 08:59:29 -0700

    Feat: Improve geometry :rocket:

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+"""Improve Geometry"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry"""
+    def area(self):
+        """
+        Function that returns a raise error
+        """
+        raise Exception("area() is not implemented")
