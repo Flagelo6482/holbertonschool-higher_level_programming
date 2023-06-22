0e366c796c6cebe244d1f8974e797357a9a333a5
Flagelo6482 <frankln6482@gmail.com>
2023-06-21 20:03:26 -0700

    Feat: Exact same object :rocket:

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+"""
+Exact same
+object
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Function that returns True if the object
+    is an instance of a specified class
+    """
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
