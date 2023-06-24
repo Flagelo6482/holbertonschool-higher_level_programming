b09ac6cad510cff24ad50cf4589c2bc7dad99481
Flagelo6482 <frankln6482@gmail.com>
2023-06-24 14:29:06 -0700

    Feat: Update :rocket:

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-
-
 """My list sorted!"""
 
 
 class MyList(list):
     """Clas that prints an ordered list"""
+
     def print_sorted(self):
         """
         Function that prints
