68879e15e4aa68aa2a98ee40cd64483e995c1da1
Flagelo6482 <frankln6482@gmail.com>
2023-06-25 16:04:05 -0700

    Feat: New files :rocket:

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+"""Save Object to a file"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Function that writes an Object to a text file"""
+    with open(filename, mode="w", encoding="utf-8") as file:
+        """
+        Args:
+            filename(file)   :File text
+            mode(mode)       :Openning mode
+            encoding(coding) :Encoding
+        """
+        file.write(json.dumps(my_obj))
