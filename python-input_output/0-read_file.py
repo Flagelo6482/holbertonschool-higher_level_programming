565d060452650516577e1b4eb4fd0a50cf9f0540
Flagelo6482 <frankln6482@gmail.com>
2023-06-25 07:21:22 -0700

    Feat: Update :rocket:

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,5 +9,10 @@ Read file
 def read_file(filename=""):
     """Function to read a file and print it"""
     with open(filename, encoding="utf-8") as file:
+        """
+        Args:
+            filename(file): File text
+            encoding(string):Coding
+        """
         var = file.read()
         print(var)
