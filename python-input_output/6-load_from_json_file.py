c11b41f890d39c74a408fe3f33d8cfa205c9fb21
Flagelo6482 <frankln6482@gmail.com>
2023-06-25 16:49:05 -0700

    Feat: Create object from a JSON file :rocket:

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+"""Create object from a JSON file"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """Function that creates an object from a "JSON file"""
+    with open(filename, mode="r", encoding="utf-8") as file:
+        """
+        Args:
+            filename(file)   :File text
+            mode(mode)       :Openning mode
+            encoding(coding) :Encoding
+        """
+        objc = json.loads(file)
