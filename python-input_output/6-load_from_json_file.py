ced7605758991cdf545037b939eaa24be37578d4
Flagelo6482 <frankln6482@gmail.com>
2023-06-25 17:02:31 -0700

    Feat: Update :rocket:

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -9,11 +9,12 @@ import json
 
 def load_from_json_file(filename):
     """Function that creates an object from a "JSON file"""
-    with open(filename, mode="r", encoding="utf-8") as file:
+    with open(filename, mode="r") as file:
         """
         Args:
             filename(file)   :File text
             mode(mode)       :Openning mode
             encoding(coding) :Encoding
         """
-        objc = json.loads(file)
+        objc = json.load(file)
+    return objc
