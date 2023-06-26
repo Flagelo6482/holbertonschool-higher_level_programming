394937ea9313c4e7777a75bc99a14dc545747a23
Flagelo6482 <frankln6482@gmail.com>
2023-06-26 16:21:47 -0700

    Feat: Update :rocket:

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -12,13 +12,12 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 file = "add_item.json"
 i = 1
-lista = []
+
+lista = load_from_json_file(file)
 
 #Agregamos todos los argumentos en una lista
 while i < len(sys.argv):
     lista.append(sys.argv[i])
     i += 1
-
-lista = load_from_json_file(file)
 
 save_to_json_file(lista, file)
