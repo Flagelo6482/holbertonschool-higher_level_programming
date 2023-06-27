ed97a31dd7eea558eaa4705c2bc18e1da6a73b33
Flagelo6482 <frankln6482@gmail.com>
2023-06-26 17:01:38 -0700

    Feat: Update :rocket:

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+"""Class to JSON"""
+
+
+import json
+
+
+def class_to_json(obj):
+    """
+    Function that returns the dictionary
+    description with simple data structure
+    """
+    return obj.__dict__
