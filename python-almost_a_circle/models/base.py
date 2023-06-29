b6de70bd30d2b23d911672c75b0840350cbbb3e3
Flagelo6482 <frankln6482@gmail.com>
2023-06-28 21:46:08 -0700

    Feat: JSON string :rocket:

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,6 +4,9 @@
 """Base class"""
 
 
+import json
+
+
 class Base:
     """Base class"""
 
@@ -21,3 +24,15 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Function that returns a dictionary in a JSON string
+        Args:
+            list_dictionaries(dict) :Dictionary
+        """
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
