e5ffd603fc2c112ab5499a782fcb59e5768e9af5
Flagelo6482 <frankln6482@gmail.com>
2023-07-01 16:35:43 -0700

    Feat: Update tests :rocket:

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -6,6 +6,11 @@ from models.base import Base
 class To_json_string(unittest.TestCase):
     """Comentario"""
 
+    def test_init(self):
+        """Comentario"""
+        obj = Base()
+        self.assertEqual(1, obj.id)
+
     def test_to_json_string_empty(self):
         """Comentario"""
         self.assertEqual("[]", Base.to_json_string([]))
