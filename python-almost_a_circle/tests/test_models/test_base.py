cbd2174e83187e9afff958b94945ef75b4bc66ed
Flagelo6482 <frankln6482@gmail.com>
2023-07-01 11:16:04 -0700

    Feat: Unittest :rocket:

---
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,8 @@
+import unittest
+
+from test_models.base import Base
+
+class UniTestBase(unittest.TestCase):
+
+    def test_to_json_string(self):
+        self.assertEqual(Base.to_json_string([]), "[]")
