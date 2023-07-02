118c856c1c9755778603ea3bd78a765b8d0286da
Flagelo6482 <frankln6482@gmail.com>
2023-07-01 17:03:40 -0700

    Feat: Update :rocket:

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,6 +1,8 @@
 """Comentario"""
 import unittest
+import json
 from models.base import Base
+from models.rectangle import Rectangle
 
 
 class To_json_string(unittest.TestCase):
@@ -8,16 +10,26 @@ class To_json_string(unittest.TestCase):
 
     def test_init(self):
         """Comentario"""
-        obj = Base()
-        self.assertEqual(1, obj.id)
+        obj_1 = Base()
+        obj_2 = Base(99)
+        self.assertEqual(1, obj_1.id)
+        self.assertEqual(99, obj_2.id)
 
-    def test_to_json_string_empty(self):
-        """Comentario"""
-        self.assertEqual("[]", Base.to_json_string([]))
-
-    def test_to_json_string_none(self):
+    def test_to_json_string(self):
         """Comentario"""
         self.assertEqual("[]", Base.to_json_string(None))
+        self.assertEqual("[]", Base.to_json_string([]))
+        js = json.dumps([{'width': 4, 'height': 5}])
+        self.assertEqual(js, Base.to_json_string([{'width': 4, 'height': 5}]))
+        self.assertEqual(str, type(js))
+
+    def test_from_json_string(self):
+        """Comentario"""
+        str_json = '[{"width": 4, "height": 5}]'
+        self.assertEqual([], Base.from_json_string(None))
+        j_s = json.loads(str_json)
+        self.assertEqual(j_s, Base.from_json_string(str_json))
+        self.assertEqual(list, type(j_s))
 
 
 if __name__ == '__main__':
