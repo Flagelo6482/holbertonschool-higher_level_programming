da83457ae103a4501ecb82be382fab9f654e1dfc
Flagelo6482 <frankln6482@gmail.com>
2023-07-01 19:24:06 -0700

    Feat: Update :rocket:

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -13,7 +13,7 @@ class To_json_string(unittest.TestCase):
         """Comentario"""
         obj_1 = Base()
         obj_2 = Base(99)
-        self.assertEqual(1, obj_1.id)
+        self.assertEqual(2, obj_1.id)
         self.assertEqual(99, obj_2.id)
 
     def test_to_json_string(self):
@@ -53,6 +53,50 @@ class To_json_string(unittest.TestCase):
         str_json = Rectangle.to_json_string(file_)
         with open("Rectangle.json", "r") as f:
             self.assertEqual(f.readline(), str_json)
+
+    def test_create(self):
+        """Test for this method"""
+        r1 = Rectangle(3, 5, 1)
+        r1_d = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_d)
+        self.assertAlmostEqual(r1.id, 1)
+        self.assertAlmostEqual(r2.id, 1)
+        self.assertAlmostEqual(r2, r2)
+        self.assertFalse(r1 == r2)
+        self.assertFalse(r1 is r2)
+
+    def test_load_from_file(self):
+        """Test for this method"""
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        list_input = [r1, r2]
+        Rectangle.save_to_file(list_input)
+        list_output = Rectangle.load_from_file()
+        self.assertAlmostEqual(list_output[0].id, 3)
+        self.assertAlmostEqual(list_output[0].x, 2)
+        self.assertAlmostEqual(list_output[0].width, 10)
+        self.assertAlmostEqual(list_output[0].height, 7)
+        self.assertAlmostEqual(list_output[0].y, 8)
+        self.assertAlmostEqual(list_output[1].id, 4)
+        self.assertAlmostEqual(list_output[1].x, 0)
+        self.assertAlmostEqual(list_output[1].width, 2)
+        self.assertAlmostEqual(list_output[1].height, 4)
+        self.assertAlmostEqual(list_output[1].y, 0)
+        s1 = Square(5)
+        s2 = Square(7, 9, 1)
+        sinput = [s1, s2]
+        Square.save_to_file(sinput)
+        soutput = Square.load_from_file()
+        self.assertAlmostEqual(soutput[0].id, 5)
+        self.assertAlmostEqual(soutput[0].width, 5)
+        self.assertAlmostEqual(soutput[0].height, 5)
+        self.assertAlmostEqual(soutput[0].x, 0)
+        self.assertAlmostEqual(soutput[0].y, 0)
+        self.assertAlmostEqual(soutput[1].id, 6)
+        self.assertAlmostEqual(soutput[1].width, 7)
+        self.assertAlmostEqual(soutput[1].height, 7)
+        self.assertAlmostEqual(soutput[1].x, 9)
+        self.assertAlmostEqual(soutput[1].y, 1)
 
 
 if __name__ == '__main__':
